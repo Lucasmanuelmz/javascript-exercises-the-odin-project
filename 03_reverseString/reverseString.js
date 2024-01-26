fde32421821b22d22656d615b714ff021b74626e
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-26 08:48:52 +0200

    Resolvi o exercício 4, desafiando minhas habilidades em javascript

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let string = '';
+  if(string < 0) return "ERROR";
+  for(let i = str.length -1; i >= 0; i--){
+    string += str[i];
+  }
+  return string;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+//exrecício 1
