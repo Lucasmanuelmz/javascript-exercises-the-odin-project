1918112ab479c59bd573f8f97e80d584d83f1df7
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-28 08:01:18 +0200

    Resolvido exercício 5.
    Nota: Este exercício foi o mais difífil que encontrei até então. Mas foi possível vencer este desafio

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  //SOLUCTIONS
+  if(!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR"
+  } else if(num1 < 0 || num2 < 0) {
+    return "ERROR"
+ }
 
+  if(num1 > num2) {
+    [num1, num2] = [num2, num1]
+  }
+
+  let result = 0;
+
+  for(let i = num1; i <= num2; i++) {
+     result += i
+  }
+  return result
 };
-
 // Do not edit below this line
 module.exports = sumAll;
+
