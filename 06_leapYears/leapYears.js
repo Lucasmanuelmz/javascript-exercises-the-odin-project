dcade1a8b547d4d49106e583df15e75242f8a443
Lucasmanuelmz <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-01-28 09:08:28 +0200

    Resolvido! Este foi amigável

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function( year) {
+    //My solutions:
+   if(!Number.isInteger(year)) {
+   return "Ano Inválido!"
+   } else {
+    return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
+   }
+    
 };
 
 // Do not edit below this line
